08f4066282f4ebfaebb4c1eb9b9b5320910dfbd4
Nicolò <nico.paganelli91@gmail.com>
2020-07-01 12:08:07 +0200

    Set html e chiamata all'API

---
+++ js/script.js
@@ -0,0 +1,60 @@
+////////// Milestone 1:
+// Creare un layout base con una searchbar (una input e un button)
+// in cui possiamo scrivere completamente o parzialmente il nome di un film.
+// Possiamo, cliccando il  bottone,
+// cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API
+// visualizzare a schermo i seguenti valori per ogni film trovato:
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+
+////////// Dettagli chiamata AJAX:
+// endpoint: https://api.themoviedb.org/3/search/movie
+// API KEY: 4cc6118b11d73c4c0274675794143586
+// query: valore della input #ricerca (film da cercare)
+// language: imposta la lingua del film cercato (it-IT)
+
+$(document).ready(
+  function() {
+
+    // Click sul bottone #cerca
+    $('#cerca').click(
+      function() {
+
+        $.ajax(
+          {
+            url: 'https://api.themoviedb.org/3/search/movie',
+            method: 'GET',
+            data: {
+              api_key: '4cc6118b11d73c4c0274675794143586',
+              query: 'ted', // TODO: $('#ricerca').val()
+              language: 'it-IT'
+            },
+            success: function(rispostAPI) {
+              console.log(rispostAPI);
+              // console.log(rispostAPI.total_results);
+              // console.log(rispostAPI.total_pages);
+              console.log(rispostAPI.results);
+
+
+            },
+            error: function() {
+              alert('errore');
+            }
+          }
+        );
+      }
+    );
+
+////////// Fine document.ready
+  }
+);
+
+// Handlebars
+// var source = $("#film-template").html();
+// var template = Handlebars.compile(source);
+//
+// var context = { title: "My New Post", body: "This is my first post!" };
+// var html = template(context);
