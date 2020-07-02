e5def9beed74ba2fbf567627e8a905f0661dca91
Nicolò <nico.paganelli91@gmail.com>
2020-07-02 12:12:53 +0200

    Funzione reset lista film e valore input e stampa errore

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+    // -------------------------- INFO APPLICAZIONE -------------------------- //
+
 ////////// Milestone 1:
 // Creare un layout base con una searchbar (una input e un button)
 // in cui possiamo scrivere completamente o parzialmente il nome di un film.
@@ -10,11 +12,33 @@
 // Lingua
 // Voto
 
+////////// Milestone 2:
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5,
+// così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+// lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva,
+// non gestiamo icone mezze piene (o mezze vuote :P)
+//
+// Trasformiamo poi la stringa statica della lingua
+// in una vera e propria bandiera della nazione corrispondente,
+// gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API
+//
+// Allarghiamo poi la ricerca anche alle serie tv.
+// Con la stessa azione di ricerca dovremo prendere
+// sia i film che corrispondono alla query, sia le serie tv,
+// stando attenti ad avere alla fine dei valori simili
+// (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Qui un esempio di chiamata per le serie tv:
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
+
 ////////// Dettagli chiamata AJAX:
 // endpoint: https://api.themoviedb.org/3/search/movie
 // API KEY: 4cc6118b11d73c4c0274675794143586
 // query: valore della input #ricerca (film da cercare)
 // language: imposta la lingua del film cercato (it-IT)
+
+    // -------------------------- FINE INFO APPLICAZIONE -------------------------- //
 
 $(document).ready(
   function() {
@@ -24,36 +48,34 @@ $(document).ready(
                         // ----- Ricerca Film ----- //
 
     ////////// Click sul bottone #cerca
+    // prendo il valore della input #ricerca
+    // e cerco i film nell'API
     $('#cerca').click(
       function() {
 
-        // resetto la lista dei film html per non accodare nuove ricerche
-        $('.lista-films').html('');
-
         // valore della input inserito nel campo ricerca
         var filmCercato = $('#ricerca').val();
-        stampaRicerca(filmCercato);
+        ottieniRicerca(filmCercato);
 
         // infine cancello il testo scritto nella input
-        $('#ricerca').val('');
+        resetInput();
       }
     );
 
     ////////// Invio #ricerca da tastiera
+    // prendo il valore della input #ricerca
+    // e cerco i film nell'API
     $('#ricerca').keypress(
       function() {
 
         if ((event.which === 13) || (event.keyCode === 13)) {
 
-          // resetto la lista dei film html per non accodare nuove ricerche
-          $('.lista-films').html('');
-
           // valore della input inserito nel campo ricerca
           var filmCercato = $('#ricerca').val();
-          stampaRicerca(filmCercato);
+          ottieniRicerca(filmCercato);
 
           // infine cancello il testo scritto nella input
-          $('#ricerca').val('');
+          resetInput();
         }
       }
     );
@@ -106,20 +128,26 @@ $(document).ready(
       }
     }
 
-    ////////// STAMPA RICERCA
+    ////////// OTTIENI RICERCA
     // Funzione che genera una chiamata Ajax
     // prende il valore inserito nella input #ricerca e lo setta come query per la chiamata
     //  --> filmCercato: stringa che serve alla funzione come argomento
     //      per generare la query all'API
     // return: niente
-    function stampaRicerca(filmCercato) {
+    function ottieniRicerca(filmCercato) {
+      resetHtml()
+
+      ////////// Info chiamata
+      // metto i dettagli della chamata Ajax in delle variabili
+      var url = 'https://api.themoviedb.org/3/search/movie';
+      var api_key = '4cc6118b11d73c4c0274675794143586';
 
       $.ajax(
         {
-          url: 'https://api.themoviedb.org/3/search/movie',
+          url: url,
           method: 'GET',
           data: {
-            api_key: '4cc6118b11d73c4c0274675794143586',
+            api_key: api_key,
             query: filmCercato,
             language: 'it-IT'
           },
@@ -128,14 +156,61 @@ $(document).ready(
             ////////// Array di ritorno chiamata Ajax
             var arrayObjFilm = rispostAPI.results;
 
-            stampaFilm(arrayObjFilm)
+            // Se l'array che mi torna è vuoto, stampo un messaggio di errore
+            if (arrayObjFilm.length > 0) {
+
+              stampaFilm(arrayObjFilm)
+            } else {
+
+              ////////// Imposto un messaggio di errore
+              var messaggioErrore = 'Errore, non è stato trovato nessun film';
+              stampaErrore(messaggioErrore);
+            }
+
           },
           error: function() {
-            alert('Inserisci il titolo di un film');
+
+            ////////// Imposto un messaggio di errore
+            var messaggioErrore = 'Errore, inserisci una parola nella ricerca film';
+            stampaErrore(messaggioErrore);
           }
         }
       );
     }
+
+    ////////// STAMPA ERRORE
+    // Funzione che stampa un messaggio di errore
+    //  --> messaggio: è una stringa che contiene il messaggio da visualizzare
+    // return: niente
+    function stampaErrore(messaggio) {
+
+      // Preparo il template handlebars
+      // a cui darò in pasto il messaggio da stampare
+      var source = $("#messaggio-template").html();
+      var template = Handlebars.compile(source);
+
+      ////////// Oggetto Handlebars
+      var errore = {
+        messaggio: messaggio
+      };
+
+      // Stampo nell'html il messaggio di errore
+      var html = template(errore);
+      $('.lista-films').append(html);
+    }
+
+    ////////// RESET HTML
+    // Funzione che svuota la lista dei film trovati per non accodare nuove ricerche
+    function resetHtml() {
+      $('.lista-films').html('');
+    }
+
+    ////////// RESET INPUT
+    // Funzione che svuota la lista dei film trovati per non accodare nuove ricerche
+    function resetInput() {
+      $('#ricerca').val('');
+    }
+
     // -------------------------- FINE FUNZIONI -------------------------- //
 
 ////////// Fine document.ready
